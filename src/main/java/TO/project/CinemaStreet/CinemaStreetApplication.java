dded04aa8b52660e4a011047e518f383086dec29
Wojciech Siwek <405956@student.agh.edu.pl>
2022-12-04 21:58:32 +0100

    Initial Commit

---
+++ src/main/java/TO/project/CinemaStreet/CinemaStreetApplication.java
@@ -0,0 +1,13 @@
+package TO.project.CinemaStreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaStreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaStreetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TO/project/CinemaStreet/CinemaStreetApplicationTests.java
@@ -0,0 +1,13 @@
+package TO.project.CinemaStreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaStreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
